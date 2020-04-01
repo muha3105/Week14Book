42c7fd24ca36808a5bd188eaf775d707d9297fb1
AljoschaEASV <aljo0025@easv365.dk>
2020-04-01 10:44:49 +0200

    Added the total interface

--- src/rentABook.java
+++ src/rentABook.java
@@ -4,8 +4,33 @@ import java.util.Observable;
 
 public interface rentABook {
 
-    void booksToRent(ObservableList allBooks, String userName, String password );
+    public void showRentals(String username);
 
-    public void addBookToRentList ();
 
+    public void book(String title, String author);
+
+
+    public void setTitle(String title);
+
+    public void getTitle();
+
+    public void setAuthor(String author);
+
+    public void getAuthor();
+
+    public void user(String userName, String password);
+
+
+    public void setUserName(String username);
+
+    public void setPassword(String password);
+
+    public void getPassword();
+
+
+    public void rentedBook(ObservableList rentedBook, String Username);
+
+    public void setRentedBook(ObservableList rentedBook);
+
+    public void getRentedBook(ObservableList rentedBook, String userName);
 }
