2c2919a36dc0c5dac8300d58e847c736a577b127
kasperdinsen <kasperdinsen@gmail.com>
2020-04-01 10:36:46 +0200

    UI

--- src/UI.java
+++ src/UI.java
@@ -1,7 +1,5 @@
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
